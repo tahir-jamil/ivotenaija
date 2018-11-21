2b9f558a1760e94799b7d6f5dde11cc785102767
Muhammad Tahir <itstahirjamil@gmail.com>
2018-11-21 16:54:25 +0500

    time line updated

--- src/app/timeline/endorsed-candidate/endorsed-candidate.component.ts
+++ src/app/timeline/endorsed-candidate/endorsed-candidate.component.ts
@@ -11,13 +11,21 @@ export class EndorsedCandidateComponent implements OnInit {
   
   imgWidth;
   imgHeight;
+  boxWidth;
+  boxHeight;
+
   constructor() { }
+  
   posts = ['a','b','c','b','c','b','c','b','c','b','c'];
+  
   ngOnInit() {
     let deviceHeight: number = platformModule.screen.mainScreen.heightDIPs;
     let deviceWidth: number = platformModule.screen.mainScreen.widthDIPs;
-    this.imgWidth = deviceWidth * 0.26;
-    this.imgHeight = deviceHeight * 0.14;
+    this.imgWidth = deviceWidth * 0.22;
+    this.imgHeight = deviceHeight * 0.13;
+
+    this.boxWidth = deviceWidth * 0.29;
+    this.boxHeight = deviceHeight * 0.14;
   }
 
 }
